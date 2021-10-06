ba6f383e7563bcacc179306795a83369b54c579f
den14-dn <den14-dn@yandex.ru>
2021-10-06 17:06:29 +0300

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,40 +1,29 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        List<String> rst = new ArrayList<>();
-
         String element = null;
 
-        try (BufferedReader log = new BufferedReader(new FileReader(source))) {
-            for (String line = log.readLine(); line != null; line = log.readLine()) {
-                String[] substrings = line.split(" ");
-                if (substrings.length < 2) {
-                    continue;
-                }
+        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+            try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(target)))) {
+                for (String line = in.readLine(); line != null; line = in.readLine()) {
+                    String[] substrings = line.split(" ");
+                    if (substrings.length < 2) {
+                        continue;
+                    }
 
-                if ((substrings[0].equals("400") || substrings[0].equals("500")) && element == null) {
-                    element = substrings[1];
-                } else if (element != null && !substrings[0].equals("400") && !substrings[0].equals("500")) {
-                    element = element.concat(";").concat(substrings[1]);
-                    rst.add(element);
-                    element = null;
+                    if ((substrings[0].equals("400") || substrings[0].equals("500")) && element == null) {
+                        element = substrings[1];
+                    } else if (element != null && !substrings[0].equals("400") && !substrings[0].equals("500")) {
+                        element = element.concat(";").concat(substrings[1]);
+                        out.println(element);
+                        element = null;
+                    }
                 }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            for (String el : rst) {
-                out.println(el);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -42,12 +31,11 @@ public class Analizy {
     }
 
     public static void main(String[] args) {
-        /*try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
             out.println("15:01:30;15:02:32");
             out.println("15:10:30;23:12:32");
         } catch (Exception e) {
             e.printStackTrace();
-        }*/
-        new Analizy().unavailable("./data/source.csv", "./data/target.csv");
+        }
     }
 }
