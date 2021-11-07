2071b78293ac81c2766193e09a5b0148042fa2e8
den14-dn <den14-dn@yandex.ru>
2021-11-07 23:19:39 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -18,11 +18,13 @@ public class EchoServer {
                      )) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("/?msg=Bye")) {
+                        if (str.contains("msg=Hello")) {
+                            out.write("Hello".getBytes());
+                        } else if (str.contains("msg=Exit")) {
                             server.close();
+                        } else if (str.contains("msg=Any")) {
+                            out.write("What".getBytes());
                         }
-                        System.out.println(str);
-                        System.out.println("--------------------");
                     }
                     out.flush();
                 }
