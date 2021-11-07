66488c533284c15d8b7646157ee25d20b8f22c79
den14-dn <den14-dn@yandex.ru>
2021-11-07 20:33:49 +0300

    7. Scanner [#504791 #225654]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,5 +1,7 @@
 package ru.job4j.io;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,33 +15,38 @@ public class CSVReader {
         Set<Integer> columnsFilters = new HashSet<>(filters.size());
         String delimiter = argsName.get("delimiter");
         boolean firstLine = true;
-        String rst = "";
+        String out = argsName.get("out");
 
         Path path = Paths.get(argsName.get("path"));
-        var scanner = new Scanner(path);
-        scanner.useDelimiter(System.lineSeparator());
-        while (scanner.hasNext()) {
-            String line = scanner.next();
-            var scannerLine = new Scanner(line);
-            scannerLine.useDelimiter(delimiter);
-            int index = 0;
-            String str = "";
-            while (scannerLine.hasNext()) {
-                String subLine = scannerLine.next();
-                if (firstLine && filters.contains(subLine)) {
-                    columnsFilters.add(index);
-                    str = str.isEmpty() ? str.concat(subLine) : str.concat(delimiter).concat(subLine);
-                } else if (!firstLine) {
-                    if (columnsFilters.contains(index)) {
+        try (var scanner = new Scanner(path);
+             FileWriter writer = new FileWriter(out)) {
+            scanner.useDelimiter(System.lineSeparator());
+            while (scanner.hasNext()) {
+                String line = scanner.next();
+                var scannerLine = new Scanner(line);
+                scannerLine.useDelimiter(delimiter);
+                int index = 0;
+                String str = "";
+                while (scannerLine.hasNext()) {
+                    String subLine = scannerLine.next();
+                    if (firstLine && filters.contains(subLine)) {
+                        columnsFilters.add(index);
                         str = str.isEmpty() ? str.concat(subLine) : str.concat(delimiter).concat(subLine);
+                    } else if (!firstLine) {
+                        if (columnsFilters.contains(index)) {
+                            str = str.isEmpty() ? str.concat(subLine) : str.concat(delimiter).concat(subLine);
+                        }
                     }
+                    index++;
                 }
-                index++;
+                firstLine = false;
+                if (out.equalsIgnoreCase("stdout")) {
+                    System.out.println(str);
+                } else {
+                    writer.write(str.concat(System.lineSeparator()));
+                }
             }
-            firstLine = false;
-            rst = rst.concat(str).concat(System.lineSeparator());
         }
-        Files.writeString(Paths.get(argsName.get("out")), rst);
     }
 
     private void validate(ArgsName argsName) {
@@ -52,5 +59,21 @@ public class CSVReader {
         } else if (argsName.get("filter") == null) {
             throw new IllegalArgumentException("No filters specified.");
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String data = String.join(
+                System.lineSeparator(),
+                "name;age;last_name;education",
+                "Tom;20;Smith;Bachelor",
+                "Jack;25;Johnson;Undergraduate",
+                "William;30;Brown;Secondary special"
+        );
+        File file = Paths.get("./source.csv").toFile();
+        ArgsName argsName = ArgsName.of(new String[]{
+                "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out=stdout", "-filter=name,age"
+        });
+        Files.writeString(file.toPath(), data);
+        CSVReader.handle(argsName);
     }
 }
