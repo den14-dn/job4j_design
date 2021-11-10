0f183f6c4eff51b891ac4cda63a8bf472d2d9048
den14-dn <den14-dn@yandex.ru>
2021-11-10 23:50:33 +0300

    3. Формат XML [#313165]

--- src/main/java/ru/job4j/serialization/Person.java
+++ src/main/java/ru/job4j/serialization/Person.java
@@ -32,23 +32,5 @@ public class Person {
 
     public static void main(String[] args) {
         final Person person = new Person("male", 30, false, new Contact("999-11-22"), "worker", "married");
-
-        final Gson gson = new GsonBuilder().create();
-        System.out.println(gson.toJson(person));
-
-        final String personJson =
-                "{"
-                        + "\"sex\":female,"
-                        + "\"age\":35,"
-                        + "\"married\":true,"
-                        + "\"contact\":"
-                        + "{"
-                        + "\"phone\":\"+7(999)123-44-55\""
-                        + "},"
-                        + "\"statuses\":"
-                        + "[\"mother\",\"wife\"]"
-                        + "}";
-        final Person personMod = gson.fromJson(personJson, Person.class);
-        System.out.println(personMod);
     }
 }
