d7c9ede5ce55a0e86d9badb942a89d37b9485053
den14-dn <den14-dn@yandex.ru>
2022-05-03 23:32:13 +0300

    0. Принцип открытости закрытости [#4914]

--- src/main/java/ru/job4j/design/ocp/ToyotaCar.java
+++ src/main/java/ru/job4j/design/ocp/ToyotaCar.java
@@ -2,12 +2,18 @@ package ru.job4j.design.ocp;
 
 import java.util.ArrayList;
 
-// 1. класс должен зависеть от абстракции
+/**
+ * 1. класс должен зависеть от абстракции
+ */
 public class ToyotaCar {
-    // 2. поля должны представлять тип абстракции
+    /**
+     * 2. поля должны представлять тип абстракции
+     */
     private GasolineEngine engine;
 
-    // 3. входные параметры и возвращаемый тип должны быть реализованы через асбтракции
+    /**
+     * 3. входные параметры и возвращаемый тип должны быть реализованы через асбтракции
+     */
     public ArrayList<Integer> getGearsToSwitch(Integer currentTransmission) {
         return null;
     }
