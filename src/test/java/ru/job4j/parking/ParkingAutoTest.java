c7a60b3589073a2ea771e181e2b3d895c56d11b3
den14-dn <den14-dn@yandex.ru>
2022-05-24 17:27:08 +0300

    2. Парковка машин [#853 #302801]

--- src/test/java/ru/job4j/parking/ParkingAutoTest.java
+++ src/test/java/ru/job4j/parking/ParkingAutoTest.java
@@ -19,11 +19,31 @@ public class ParkingAutoTest {
     }
 
     @Test
+    public void whenParkTruckAndCar() {
+        Parking parking = new ParkingAuto(2, 1);
+        parking.park(new Truck(2));
+        assertTrue(parking.park(new Car()));
+    }
+
+    @Test
     public void whenDoNotParkTruckOfSize2() {
         Parking parking = new ParkingAuto(2, 1);
-        parking.park(new Car());
-        parking.park(new Car());
-        parking.park(new Truck(5));
+        assertTrue(parking.park(new Car()));
+        assertTrue(parking.park(new Car()));
+        assertTrue(parking.park(new Truck(5)));
         assertFalse(parking.park(new Truck(2)));
+    }
+
+    @Test
+    public void whenDoNotParkCar() {
+        Parking parking = new ParkingAuto(0, 1);
+        assertTrue(parking.park(new Truck(5)));
+        assertFalse(parking.park(new Car()));
+    }
+
+    @Test
+    public void whenDoNotParkTruckOnSpaceCars() {
+        Parking parking = new ParkingAuto(3, 0);
+        assertFalse(parking.park(new Truck(4)));
     }
 }
