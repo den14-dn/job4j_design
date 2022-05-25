ba2d5b0e62c706412486b235d466c7839092532a
den14-dn <den14-dn@yandex.ru>
2022-05-25 09:26:26 +0300

    2. Парковка машин [#853 #302801]

--- src/main/java/ru/job4j/parking/Car.java
+++ src/main/java/ru/job4j/parking/Car.java
@@ -1,15 +1,10 @@
 package ru.job4j.parking;
 
 public class Car implements Auto {
-    private int size;
     public static final int SIZE_CAR = 1;
-
-    public Car() {
-        this.size = Car.SIZE_CAR;
-    }
 
     @Override
     public int getSize() {
-        return size;
+        return SIZE_CAR;
     }
 }
